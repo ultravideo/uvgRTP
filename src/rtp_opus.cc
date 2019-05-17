509d096b1da2932d6473162bfc89e4a86f3c4650
Aaro Altonen <aaro.altonen@tuni.fi>
2019-05-17 13:09:27 +0300

    Rewrite sending Opus using the new send API

--- src/rtp_opus.cc
+++ src/rtp_opus.cc
@@ -4,12 +4,19 @@
 #include "conn.hh"
 #include "debug.hh"
 #include "rtp_opus.hh"
-#include "rtp_generic.hh"
+#include "send.hh"
 
+/* Validity of arguments is checked in RTPSender. We just need to relay them there */
 int RTPOpus::pushOpusFrame(RTPConnection *conn, uint8_t *data, uint32_t dataLen, uint32_t timestamp)
 {
-    if (!conn || !data)
-        return RTP_INVALID_VALUE;
+    int ret;
+
+    if ((ret = RTPSender::writeRTPHeader(conn)) != RTP_OK) {
+        LOG_ERROR("Failed to write RTP Header for Opus frame!");
+        return ret;
+    }
+
+    return RTPSender::writePayload(conn, data, dataLen);
 
 #if 0
     uint8_t buffer[MAX_PACKET]  = { 0 };
@@ -28,8 +35,8 @@ int RTPOpus::pushOpusFrame(RTPConnection *conn, uint8_t *data, uint32_t dataLen,
     memcpy(&buffer[1], data, dataLen);
 
     return RTPGeneric::pushGenericFrame(conn, buffer, dataLen + 1, 0);
-#endif
 
     conn->setPayloadType(RTP_FORMAT_OPUS);
     return RTPGeneric::pushGenericFrame(conn, data, dataLen, 0);
+#endif
 }
