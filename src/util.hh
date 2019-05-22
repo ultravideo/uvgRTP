732ecaf43a17c121b6f24251ea45987150181898
Aaro Altonen <aaro.altonen@tuni.fi>
2019-05-22 09:39:16 +0300

    Remove rtp uniq identifier generator from util.cc

--- src/util.hh
+++ src/util.hh
@@ -3,8 +3,6 @@
 #include <cstdint>
 #include <cstddef>
 
-class RTPConnection;
-
 const int MAX_PACKET      = 65536;
 const int MAX_PAYLOAD     = 1000;
 
@@ -12,24 +10,17 @@ const size_t RTP_HEADER_SIZE      = 12;
 const size_t HEVC_RTP_HEADER_SIZE =  2;
 const size_t HEVC_FU_HEADER_SIZE  =  1;
 
-enum RTP_ERROR {
+typedef enum RTP_ERROR {
     RTP_OK            =  0,
     RTP_GENERIC_ERROR = -1,
     RTP_SOCKET_ERROR  = -2,
     RTP_BIND_ERROR    = -3,
     RTP_INVALID_VALUE = -4,
     RTP_SEND_ERROR    = -5,
-};
+} rtp_error_t;
 
 typedef enum RTP_FORMAT {
     RTP_FORMAT_GENERIC = 0,
     RTP_FORMAT_HEVC    = 96,
     RTP_FORMAT_OPUS    = 97,
 } rtp_format_t;
-
-
-static inline uint64_t rtpGetUniqueId()
-{
-    static uint64_t i = 1;
-    return i++;
-}
