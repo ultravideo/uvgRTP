67594f7e72d65b2768975008d7736fa9b006fe3e
Aaro Altonen <aaro.altonen@tuni.fi>
2019-06-03 11:58:53 +0300

    Add HEVC frame processing function
    This function merges the HEVC fragmentation units and returns full
    frames when all frag units have been received.

--- src/rtp_hevc.cc
+++ src/rtp_hevc.cc
@@ -7,7 +7,7 @@
 #include "rtp_hevc.hh"
 #include "send.hh"
 
-static int __internal_get_next_frame_start(uint8_t *data, uint32_t offset, uint32_t data_len, uint8_t& start_len)
+static int __get_next_frame_start(uint8_t *data, uint32_t offset, uint32_t data_len, uint8_t& start_len)
 {
     uint8_t zeros = 0;
     uint32_t pos  = 0;
@@ -29,19 +29,19 @@ static int __internal_get_next_frame_start(uint8_t *data, uint32_t offset, uint3
     return -1;
 }
 
-static rtp_error_t __internal_push_hevc_frame(kvz_rtp::connection *conn, uint8_t *data, size_t data_len, uint32_t timestamp)
+static rtp_error_t __push_hevc_frame(kvz_rtp::connection *conn, uint8_t *data, size_t data_len, uint32_t timestamp)
 {
-    uint32_t data_pos  = 0;
-    uint32_t data_left = data_len;
-    rtp_error_t ret    = RTP_OK;
-    uint8_t nalType    = (data[0] >> 1) & 0x3F;
+    uint8_t nalType  = (data[0] >> 1) & 0x3F;
+    rtp_error_t ret  = RTP_OK;
+    size_t data_left = data_len;
+    size_t data_pos  = 0;
 
     if (data_len <= MAX_PAYLOAD) {
-        LOG_DEBUG("send unfrag size %u, type %u", data_len, nalType);
+        LOG_DEBUG("send unfrag size %zu, type %u", data_len, nalType);
         return kvz_rtp::generic::push_generic_frame(conn, data, data_len, timestamp);
     }
 
-    LOG_DEBUG("send frag size: %u, type %u", data_len, nalType);
+    LOG_DEBUG("send frag size: %zu, type %u", data_len, nalType);
 
     uint8_t header[
         kvz_rtp::frame::HEADER_SIZE_RTP      +
@@ -83,14 +83,14 @@ rtp_error_t kvz_rtp::hevc::push_hevc_frame(kvz_rtp::connection *conn, uint8_t *d
 {
     uint8_t start_len;
     int32_t prev_offset = 0;
-    int offset = __internal_get_next_frame_start(data, 0, data_len, start_len);
+    int offset = __get_next_frame_start(data, 0, data_len, start_len);
     prev_offset = offset;
 
     while (offset != -1) {
-        offset = __internal_get_next_frame_start(data, offset, data_len, start_len);
+        offset = __get_next_frame_start(data, offset, data_len, start_len);
 
         if (offset > 4 && offset != -1) {
-            if (__internal_push_hevc_frame(conn, &data[prev_offset], offset - prev_offset - start_len, timestamp) == -1)
+            if (__push_hevc_frame(conn, &data[prev_offset], offset - prev_offset - start_len, timestamp) == -1)
                 return RTP_GENERIC_ERROR;
 
             prev_offset = offset;
@@ -100,5 +100,124 @@ rtp_error_t kvz_rtp::hevc::push_hevc_frame(kvz_rtp::connection *conn, uint8_t *d
     if (prev_offset == -1)
         prev_offset = 0;
 
-    return __internal_push_hevc_frame(conn, &data[prev_offset], data_len - prev_offset, timestamp);
+    return __push_hevc_frame(conn, &data[prev_offset], data_len - prev_offset, timestamp);
+}
+
+kvz_rtp::frame::rtp_frame *kvz_rtp::hevc::process_hevc_frame(
+        kvz_rtp::frame::rtp_frame *frame,
+        std::vector<kvz_rtp::frame::rtp_frame *>& fu,
+        rtp_error_t& error
+)
+{
+    size_t total_len               = 0;
+    bool last_frag                 = false;
+    bool first_frag                = false;
+    uint8_t NALHeader[2]           = { 0 };
+    kvz_rtp::frame::rtp_frame *ret = nullptr;
+
+    if (!frame) {
+        LOG_ERROR("Invalid value, unable to process frame!");
+
+        error = RTP_INVALID_VALUE;
+        return nullptr;
+    }
+
+    if ((frame->payload[0] >> 1) != 49) {
+        LOG_DEBUG("frame is not fragmented, returning...");
+        error = RTP_OK;
+
+        /* we received a non-fragmented HEVC frame but FU buffer is not empty 
+         * which means a packet was lost and we must drop this partial frame */
+        if (!fu.empty()) {
+            /* TODO: update rtcp stats for dropped packets */
+
+            LOG_WARN("fragmented frame was not fully received, dropping...");
+            ret = frame;
+            goto error;
+        }
+
+        return frame;
+    }
+
+    if (!fu.empty()) {
+        if (frame->timestamp != fu.at(0)->timestamp) {
+            LOG_ERROR("Timestamp mismatch for fragmentation units!");
+
+            /* TODO: update rtcp stats for dropped packets */
+
+            /* push the frame to fu vector so deallocation is cleaner */
+            fu.push_back(frame);
+            goto error;
+        }
+    }
+
+    first_frag = frame->payload[2] & 0x80;
+    last_frag  = frame->payload[2] & 0x40;
+
+    if (first_frag && last_frag) {
+        LOG_ERROR("Invalid combination of S and E bits");
+
+        /* push the frame to fu vector so deallocation is cleaner */
+        fu.push_back(frame);
+
+        error = RTP_INVALID_VALUE;
+        goto error;
+    }
+
+    frame->payload     += (kvz_rtp::frame::HEADER_SIZE_HEVC_RTP + kvz_rtp::frame::HEADER_SIZE_HEVC_FU);
+    frame->payload_len -= (kvz_rtp::frame::HEADER_SIZE_HEVC_RTP + kvz_rtp::frame::HEADER_SIZE_HEVC_FU);
+    fu.push_back(frame);
+
+    if (!last_frag) {
+        error = RTP_NOT_READY;
+        return frame;
+    }
+
+    if (fu.size() == 1) {
+        LOG_ERROR("Received the last FU but FU vector is empty!");
+        error = RTP_INVALID_VALUE;
+        goto error;
+    }
+
+    for (auto& i : fu) {
+        total_len += i->payload_len;
+    }
+
+    ret = kvz_rtp::frame::alloc_frame(total_len + 2, kvz_rtp::frame::FRAME_TYPE_GENERIC);
+
+    /* copy the RTP header of the first fragmentation unit and use it for the full frame */
+    memcpy(frame->data, fu.at(0)->data, kvz_rtp::frame::HEADER_SIZE_RTP);
+
+    NALHeader[0] = (fu.at(0)->payload[0] & 0x81) | ((fu.at(0)->payload[2] & 0x3f) << 1);
+    NALHeader[1] = fu.at(0)->payload[1];
+
+    memcpy(ret->payload, NALHeader, sizeof(NALHeader));
+    ret->payload += sizeof(NALHeader);
+
+    {
+        size_t ptr = 0;
+
+        for (auto& i : fu) {
+            memcpy(&ret->payload[ptr], i->payload, i->payload_len);
+            ptr += i->payload_len;
+        }
+    }
+
+    while (fu.size() != 0) {
+        auto tmp = fu.back();
+        fu.pop_back();
+        kvz_rtp::frame::dealloc_frame(tmp);
+    }
+
+    error = RTP_OK;
+    return ret;
+
+error:
+    if (!fu.empty()) {
+        for (auto& i: fu) {
+            (void)kvz_rtp::frame::dealloc_frame(i);
+        }
+    }
+
+    return ret;
 }
--- src/rtp_hevc.hh
+++ src/rtp_hevc.hh
@@ -7,5 +7,21 @@ namespace kvz_rtp {
 
     namespace hevc {
         rtp_error_t push_hevc_frame(kvz_rtp::connection *conn, uint8_t *data, size_t data_len, uint32_t timestamp);
+
+        /* Process the incoming HEVC frame
+         * The RTP frame "frame" given as parameter should be considered invalid after calling this function
+         * and no operatios should be performed on it after the function has returned.
+         *
+         * On success, a valid RTP frame is returned and "error" is set to RTP_OK
+         *
+         * If the original frame has been split and this is a fragment of it, the fragment is returned
+         * and "error" is set to RTP_NOT_READY
+         *
+         * If the frame is invalid, nullptr is returned and "error" is set to RTP_INVALID_VALUE (is possible) */
+        kvz_rtp::frame::rtp_frame *process_hevc_frame(
+            kvz_rtp::frame::rtp_frame *frame,
+            std::vector<kvz_rtp::frame::rtp_frame *>& fu,
+            rtp_error_t& error
+        );
     };
 };
