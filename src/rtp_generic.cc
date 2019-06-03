72313a783df0a85dce7079d7480b0cb70e6dedb9
Aaro Altonen <aaro.altonen@tuni.fi>
2019-06-03 11:57:25 +0300

    Add generic frame processing fuction

--- src/rtp_generic.cc
+++ src/rtp_generic.cc
@@ -21,7 +21,7 @@ rtp_error_t kvz_rtp::generic::push_generic_frame(connection *conn, uint8_t *data
     rtp_error_t ret;
 
     if (data_len > MAX_PAYLOAD)
-        LOG_WARN("packet is larger (%zu bytes) than MAX_PAYLOAD (%zu bytes)", data_len, MAX_PAYLOAD);
+        LOG_WARN("packet is larger (%zu bytes) than MAX_PAYLOAD (%u bytes)", data_len, MAX_PAYLOAD);
 
     if ((ret = kvz_rtp::sender::write_rtp_header(conn, timestamp)) != RTP_OK) {
         LOG_ERROR("Failed to write RTP Header for Opus frame!");
@@ -29,4 +29,22 @@ rtp_error_t kvz_rtp::generic::push_generic_frame(connection *conn, uint8_t *data
     }
 
     return kvz_rtp::sender::write_payload(conn, data, data_len);
+}
+
+kvz_rtp::frame::rtp_frame *kvz_rtp::generic::process_generic_frame(
+    kvz_rtp::frame::rtp_frame *frame,
+    std::vector<kvz_rtp::frame::rtp_frame *>& fu,
+    rtp_error_t& error
+)
+{
+    (void)fu;
+
+    if (!frame) {
+        error = RTP_INVALID_VALUE;
+
+        LOG_ERROR("Invalid value, unable to process frame!");
+        return nullptr;
+    }
+
+    return nullptr;
 }
