0ed923e5a02e06844fbbe473031616c1edf531a5
Aaro Altonen <aaro.altonen@tuni.fi>
2019-07-19 09:31:07 +0300

    Implement frame queue for windows
    This isn't quite optimal solution just yet. All copying should be
    deleted but that requires some extensive studying of the winsock
    documentation and it has to do for now

--- src/queue.cc
+++ src/queue.cc
@@ -9,10 +9,13 @@
 #include "debug.hh"
 #include "queue.hh"
 #include "writer.hh"
+#include <iostream>
 
 kvz_rtp::frame_queue::frame_queue()
 #ifdef __linux__
     :hdr_ptr_(0), msg_ptr_(0), chunk_ptr_(0)
+#else
+    :buf_ptr_(0)
 #endif
 {
 }
@@ -36,6 +39,7 @@ rtp_error_t kvz_rtp::frame_queue::enqueue_message(
         return RTP_MEMORY_ERROR;
     }
 
+    /* TODO: this is not valid after return */
     sockaddr_in out_addr = dynamic_cast<kvz_rtp::writer *>(conn)->get_out_address(); 
 
     chunks_[chunk_ptr_ + 0].iov_base = header;
@@ -56,14 +60,29 @@ rtp_error_t kvz_rtp::frame_queue::enqueue_message(
     chunk_ptr_ += 2;
     msg_ptr_   += 1;
     hdr_ptr_   += 1;
+#else
+    (void)conn;
+
+    if  (!header || header_len == 0 || !payload || payload_len == 0)
+        return RTP_INVALID_VALUE;
+
+    if (buf_ptr_ + 1 >= MAX_CHUNK_COUNT)
+        return RTP_MEMORY_ERROR;
+
+    uint8_t *tmp = new uint8_t[header_len + payload_len];
+
+    memcpy(tmp,              header,  header_len);
+    memcpy(tmp + header_len, payload, payload_len);
+
+    buffers_[buf_ptr_].buf = (char *)tmp;
+    buffers_[buf_ptr_].len = (u_long)(header_len + payload_len);
+
+    merge_bufs_.push_back(tmp);
+    buf_ptr_++;
+#endif
 
     conn->inc_rtp_sequence();
     conn->inc_sent_pkts();
-#else
-    /* TODO: winsock */
-
-    (void)conn, (void)header,(void)header_len, (void)payload, (void)payload_len;
-#endif
 
     return RTP_OK;
 }
@@ -82,6 +101,7 @@ rtp_error_t kvz_rtp::frame_queue::enqueue_message(
         return RTP_MEMORY_ERROR;
     }
 
+    /* TODO: this is not valid after return */
     sockaddr_in out_addr = dynamic_cast<kvz_rtp::writer *>(conn)->get_out_address(); 
 
     chunks_[chunk_ptr_ + 0].iov_base = message;
@@ -99,14 +119,23 @@ rtp_error_t kvz_rtp::frame_queue::enqueue_message(
     chunk_ptr_++;
     msg_ptr_++;
     hdr_ptr_++;
+#else
+    (void)conn;
+
+    if (!message || !message_len == 0)
+        return RTP_INVALID_VALUE;
+
+    if (buf_ptr_ + 1 >= MAX_CHUNK_COUNT)
+        return RTP_MEMORY_ERROR;
+
+    buffers_[buf_ptr_].len = (u_long)message_len;
+    buffers_[buf_ptr_].buf = (char *)message;
+
+    buf_ptr_++;
+#endif
 
     conn->inc_rtp_sequence();
     conn->inc_sent_pkts();
-#else
-    /* TODO: winsock */
-
-    (void)conn, (void)message, (void)message_len;
-#endif
 
     return RTP_OK;
 }
@@ -130,6 +159,7 @@ rtp_error_t kvz_rtp::frame_queue::enqueue_message(
         chunks_[chunk_ptr_ + i].iov_base = buffers.at(i).second;
     }
 
+    /* TODO: this is not valid after return */
     sockaddr_in out_addr = dynamic_cast<kvz_rtp::writer *>(conn)->get_out_address(); 
 
     messages_[msg_ptr_].msg_name = (void *)&out_addr;
@@ -144,14 +174,36 @@ rtp_error_t kvz_rtp::frame_queue::enqueue_message(
     chunk_ptr_ += buffers.size();
     msg_ptr_   += 1;
     hdr_ptr_   += 1;
+#else
+    (void)conn;
+    DWORD sent_bytes;
+
+    if (buf_ptr_ + buffers.size() >= MAX_CHUNK_COUNT)
+        return RTP_MEMORY_ERROR;
+
+    unsigned ptr = 0, total_size = 0;
+    uint8_t *tmp = nullptr;
+
+    for (size_t i = 0; i < buffers.size(); ++i) {
+        total_size += buffers.at(i).first;
+    }
+
+    tmp = new uint8_t[total_size];
+
+    for (size_t i = 0; i < buffers.size(); ++i) {
+        mempcpy(tmp + ptr, buffers.at(i).second, buffers.at(i).first);
+        ptr += buffers.at(i).first;
+    }
+
+    buffers_[buf_ptr_].buf = (char *)tmp;
+    buffers_[buf_ptr_].len = (u_long)total_size;
+
+    merge_bufs_.push_back(tmp);
+    buf_ptr_++;
+#endif
 
     conn->inc_rtp_sequence();
     conn->inc_sent_pkts();
-#else
-    /* TODO: winsock */
-
-    (void)conn, (void)buffers;
-#endif
 
     return RTP_OK;
 }
@@ -161,34 +213,52 @@ rtp_error_t kvz_rtp::frame_queue::flush_queue(kvz_rtp::connection *conn)
 #ifdef __linux__
     rtp_error_t ret = RTP_OK;
 
-    if (msg_ptr_ == 0 || hdr_ptr_ == 0 || chunk_ptr_ == 0) {
+    if (!conn || msg_ptr_ == 0 || hdr_ptr_ == 0 || chunk_ptr_ == 0) {
         LOG_ERROR("Cannot send 0 messages or messages containing 0 chunks!");
-        ret = RTP_INVALID_VALUE;
-        goto end;
+        empty_queue();
+        return RTP_INVALID_VALUE;
     }
 
     if (sendmmsg(conn->get_raw_socket(), headers_, hdr_ptr_, 0) < 0) {
         LOG_ERROR("Failed to flush the message queue!");
-        ret = RTP_SEND_ERROR;
+        empty_queue();
+        return RTP_SEND_ERROR;
     }
 
     LOG_DEBUG("full message took %d chunks and %d messages", chunk_ptr_, msg_ptr_);
 
-end:
     return kvz_rtp::frame_queue::empty_queue();
 #else
-    /* TODO: winsock */
+    if (!conn || buf_ptr_ == 0)
+        return RTP_INVALID_VALUE;
 
-    (void)conn;
+    sockaddr_in addr = dynamic_cast<kvz_rtp::writer *>(conn)->get_out_address();
+    DWORD sent_bytes;
+
+    if (WSASendTo(conn->get_raw_socket(), buffers_, buf_ptr_, &sent_bytes, 0, (SOCKADDR *)&addr, sizeof(addr), NULL, NULL) == -1) {
+        win_get_last_error();
+        empty_queue();
+        return RTP_SEND_ERROR;
+    }
+
+    std::cerr << std::endl << "FLUSHING QUEUE!" << std::endl;
+    LOG_INFO("full message took %d buffers, sent %u bytes", buf_ptr_, sent_bytes);
+    return empty_queue();
 #endif
-
-    return RTP_OK;
 }
 
 rtp_error_t kvz_rtp::frame_queue::empty_queue()
 {
 #ifdef __linux__
     hdr_ptr_ = msg_ptr_ = chunk_ptr_ = 0;
+#else
+    buf_ptr_ = 0;
+
+    for (size_t i = 0; i < merge_bufs_.size(); ++i) {
+        auto buffer = merge_bufs_.back();
+        merge_bufs_.pop_back();
+        delete[] buffer;
+    }
 #endif
 
     return RTP_OK;
--- src/queue.hh
+++ src/queue.hh
@@ -7,32 +7,57 @@
 
 namespace kvz_rtp {
 
-    const int MAX_MSG_COUNT   = 5000;
-    const int MAX_CHUNK_COUNT = 10000;
+    const int MAX_MSG_COUNT   = 2500;
+    const int MAX_CHUNK_COUNT = 5000;
 
     class frame_queue {
     public:
         frame_queue();
         ~frame_queue();
 
+        /* Cache "header" and "payload" to frame queue
+         * Header is placed first in the queue and then the payload
+         *
+         * Return RTP_OK on success
+         * Return RTP_INVALID_VALUE if one of the parameters is invalid
+         * Return RTP_MEMORY_ERROR if the maximum amount of chunks/messages is exceeded */
         rtp_error_t enqueue_message(
             kvz_rtp::connection *conn,
             uint8_t *header,  size_t header_len,
             uint8_t *payload, size_t payload_len
         );
 
+        /* Cache "message" to frame queue
+         * If this is the first message to be cached, is should be an RTP header
+         *
+         * Return RTP_OK on success
+         * Return RTP_INVALID_VALUE if one of the parameters is invalid
+         * Return RTP_MEMORY_ERROR if the maximum amount of chunks/messages is exceeded */
         rtp_error_t enqueue_message(
             kvz_rtp::connection *conn,
             uint8_t *message, size_t message_len
         );
 
+        /* Cache all messages in "buffers" in order to frame queue
+         * If this is the first message to be cached, the first buffer
+         * of "buffers" should point to an RTP header
+         *
+         * Return RTP_OK on success
+         * Return RTP_INVALID_VALUE if one of the parameters is invalid
+         * Return RTP_MEMORY_ERROR if the maximum amount of chunks/messages is exceeded */
         rtp_error_t enqueue_message(
             kvz_rtp::connection *conn,
             std::vector<std::pair<size_t, uint8_t *>>& buffers
         );
 
+        /* Flush the message queue
+         *
+         * Return RTP_OK on success
+         * Return RTP_INVALID_VALUE if "conn" is nullptr or message buffer is empty
+         * return RTP_SEND_ERROR if send fails */
         rtp_error_t flush_queue(kvz_rtp::connection *conn);
 
+        /* Set all pointers to 0 and free any memory */
         rtp_error_t empty_queue();
 
     private:
@@ -45,7 +70,9 @@ namespace kvz_rtp {
     int msg_ptr_;
     int chunk_ptr_;
 #else
-    /* TODO: winsock */
+    std::vector<uint8_t *> merge_bufs_;
+    WSABUF buffers_[MAX_MSG_COUNT];
+    int buf_ptr_;
 #endif
     };
 };
