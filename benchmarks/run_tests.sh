7242be636cf3053ca3e59f357e69f0641a2da09b
Aaro Altonen <aaro.altonen@tuni.fi>
2019-09-06 10:41:57 +0300

    Create test script

---
+++ benchmarks/run_tests.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+make clean && make all -j8
+nohup nc -kluvw 0 localhost 8888 &> /dev/null &
+
+for ((i = 0; i < 1; ++i)); do
+	./main_kvzrtp   &> results/kvzrtp
+	./main_jrtp     &> results/jrtp
+	./main_ccrtp    &> results/ccrtp
+	./main_live555  &> results/live555
+	./main_ortp     &> results/ortp
+done
