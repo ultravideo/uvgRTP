8f2ae2063c9857bbf7535602a5fc7c913595f32a
Aaro Altonen <aaro.altonen@tuni.fi>
2019-10-24 08:05:55 +0300

    Create RTCP hooking and polling examples

--- examples/simple/rtcp/rtcp_hook.cc
+++ examples/simple/rtcp/rtcp_hook.cc
@@ -1,19 +1,45 @@
 #include <kvzrtp/lib.hh>
 #include <cstring>
 
+/* kvzRTP calls this hook when it receives an RTCP Sender Report
+ * In this example, this doesn't get called because there's only one sender
+ *
+ * NOTE: If application uses hook, it must also free the frame when it's done with i
+ * Frame must deallocated using kvz_rtp::frame::dealloc_frame() function */
+void sender_hook(kvz_rtp::frame::rtcp_sender_frame *frame)
+{
+    fprintf(stderr, "Got RTCP Sender Report\n");
+
+    (void)kvz_rtp::frame::dealloc_frame(frame);
+}
+/* kvzRTP calls this hook when it receives an RTCP Receiver Report
+ *
+ * NOTE: If application uses hook, it must also free the frame when it's done with i
+ * Frame must deallocated using kvz_rtp::frame::dealloc_frame() function */
+void receiver_hook(kvz_rtp::frame::rtcp_receiver_frame *frame)
+{
+    fprintf(stderr, "got RTCP Receiver Report\n");
+
+    (void)kvz_rtp::frame::dealloc_frame(frame);
+}
+
 int main(void)
 {
-    /* See rtp/sending.cc for information about session initialization */
+    /* See rtp/sending.cc for more information about session initialization */
     kvz_rtp::context rtp_ctx;
 
-    kvz_rtp::writer *writer = rtp_ctx.create_writer("127.0.0.1", 8888);
-    kvz_rtp::reader *reader = rtp_ctx.create_reader("127.0.0.1", 8888);
+    kvz_rtp::writer *writer = rtp_ctx.create_writer("127.0.0.1", 8888, RTP_FORMAT_GENERIC);
+    kvz_rtp::reader *reader = rtp_ctx.create_reader("127.0.0.1", 8888, RTP_FORMAT_GENERIC);
 
-    /* Send Sender Reports 127.0.0.1:8889 and receive RTCP Reports to 5566 */
+    /* Send Sender Reports 127.0.0.1:8889 and receive RTCP Reports to 5566
+     * Add hook for the Receiver Report */
     (void)writer->create_rtcp("127.0.0.1", 8889, 5566);
+    (void)writer->get_rtcp()->install_receiver_hook(receiver_hook);
 
-    /* Send Receiver Reports 127.0.0.1:5566 and receive RTCP Reports to 8889 */
+    /* Send Receiver Reports 127.0.0.1:5566 and receive RTCP Reports to 8889
+     * and add hook for the Sender Report */
     (void)reader->create_rtcp("127.0.0.1", 5566, 8889);
+    (void)reader->get_rtcp()->install_sender_hook(sender_hook);
 
     (void)writer->start();
     (void)reader->start();
@@ -23,7 +49,7 @@ int main(void)
     memset(buffer, 'a', 50);
 
     while (true) {
-        writer->push_frame((uint8_t *)buffer, 50, RTP_FORMAT_GENERIC);
+        writer->push_frame((uint8_t *)buffer, 50, RTP_NO_FLAGS);
         std::this_thread::sleep_for(std::chrono::milliseconds(2000));
     }
 }
--- examples/simple/rtcp/rtcp_poll.cc
+++ examples/simple/rtcp/rtcp_poll.cc
@@ -1,19 +1,55 @@
 #include <kvzrtp/lib.hh>
 #include <cstring>
 
+/* The poller is run in a separate thread to prevent blocking of main thread
+ * Polling is not the advised way of handling RTCP packet retrival
+ *
+ * See rtcp/rtcp_hook.cc */
+void rtcp_thread(kvz_rtp::writer *writer)
+{
+    fprintf(stderr, "Staring RTCP Receiver Report poller...\n");
+
+    kvz_rtp::rtcp *rtcp = writer->get_rtcp();
+
+    while (true) {
+        /* Participants must be polled on
+         * every iterations as people may join/leave at any point */
+        auto ssrcs = rtcp->get_participants();
+
+        for (auto& i : ssrcs) {
+            auto report_block = rtcp->get_receiver_packet(i);
+
+            /* If a Receiver Report is received, it must be deallocated manually by
+             * calling kvz_rtp::frame::dealloc_frame() */
+            if (report_block) {
+                fprintf(stderr, "got receiver packet\n");
+                (void)kvz_rtp::frame::dealloc_frame(report_block);
+            }
+        }
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    }
+}
+
 int main(void)
 {
     /* See rtp/sending.cc for information about session initialization */
     kvz_rtp::context rtp_ctx;
 
-    kvz_rtp::writer *writer = rtp_ctx.create_writer("127.0.0.1", 8888);
-    kvz_rtp::reader *reader = rtp_ctx.create_reader("127.0.0.1", 8888);
+    kvz_rtp::writer *writer = rtp_ctx.create_writer("127.0.0.1", 8888, RTP_FORMAT_GENERIC);
+    kvz_rtp::reader *reader = rtp_ctx.create_reader("127.0.0.1", 8888, RTP_FORMAT_GENERIC);
 
     /* Send Sender Reports 127.0.0.1:8889 and receive RTCP Reports to 5566 */
     (void)writer->create_rtcp("127.0.0.1", 8889, 5566);
 
     /* Send Receiver Reports 127.0.0.1:5566 and receive RTCP Reports to 8889 */
     (void)reader->create_rtcp("127.0.0.1", 5566, 8889);
+
+    /* Create separate thread for polling the RTCP packets. In this example,
+     * we only create thread for RTCP Receiver Reports (so for RTP Sender).
+     *
+     * Polling must be obviously done in a separate thread because it's a blocking operation */
+    std::thread *t1 = new std::thread(rtcp_thread, writer);
 
     (void)writer->start();
     (void)reader->start();
@@ -23,7 +59,7 @@ int main(void)
     memset(buffer, 'a', 50);
 
     while (true) {
-        writer->push_frame((uint8_t *)buffer, 50, RTP_FORMAT_GENERIC);
+        writer->push_frame((uint8_t *)buffer, 50, RTP_NO_FLAGS);
         std::this_thread::sleep_for(std::chrono::milliseconds(2000));
     }
 }
