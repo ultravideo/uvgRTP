4ce0eecec9deda0403e2da66a7417f7cb4d08440
Aaro Altonen <aaro.altonen@tuni.fi>
2019-11-01 08:37:29 +0200

    Rewrite kvzRTP sender/receiver benchmarking code
    Instead of both files containing both receiver and sender,
    separate them into their own files

--- benchmarks/receiving/kvzrtp.cc
+++ benchmarks/receiving/kvzrtp.cc
@@ -4,72 +4,48 @@
 
 #include <easy/profiler.h>
 
-using std::chrono::high_resolution_clock;
-
 extern void *get_mem(int argc, char **argv, size_t& len);
+
+size_t bytes_sent     = 0;
+size_t bytes_received = 0;
 
 void recv_hook(void *arg, kvz_rtp::frame::rtp_frame *frame)
 {
+    (void)arg;
+
+    bytes_received += frame->payload_len;
+
+    fprintf(stderr, "%zu\n", bytes_received);
+
     (void)kvz_rtp::frame::dealloc_frame(frame);
+}
+
+void runner(kvz_rtp::context *rtp_ctx, int n, void *mem, size_t len)
+{
+    (void)mem, (void)len;
+
+    kvz_rtp::reader *reader = rtp_ctx->create_reader("127.0.0.1", 8888 + n, RTP_FORMAT_HEVC);
+    reader->install_recv_hook(NULL, recv_hook);
+    (void)reader->start();
+
+    for (;;)
+        std::this_thread::sleep_for(std::chrono::milliseconds(10000));
 }
 
 int main(int argc, char **argv)
 {
-    EASY_PROFILER_ENABLE;
+    /* EASY_PROFILER_ENABLE; */
 
     size_t len = 0;
     void *mem  = get_mem(argc, argv, len);
 
     kvz_rtp::context rtp_ctx;
 
-    kvz_rtp::writer *writer = rtp_ctx.create_writer("127.0.0.1", 8888, RTP_FORMAT_HEVC);
-    kvz_rtp::reader *reader  = rtp_ctx.create_reader("127.0.0.1", 8888, RTP_FORMAT_HEVC);
-
-    reader->install_recv_hook(NULL, recv_hook);
-
-    (void)reader->start();
-    (void)writer->start();
-
-    uint64_t chunk_size, total_size;
-    rtp_error_t ret;
-    uint64_t fpt_ms = 0;
-    uint64_t fsize  = 0;
-    uint32_t frames = 0;
-    uint64_t bytes  = 0;
-    std::chrono::high_resolution_clock::time_point start, fpt_start, fpt_end, end;
-    start = std::chrono::high_resolution_clock::now();
-
-    for (size_t i = 0; i < len; ) {
-        memcpy(&chunk_size, (uint8_t *)mem + i, sizeof(uint64_t));
-
-        i          += sizeof(uint64_t);
-        total_size += chunk_size;
-
-        fpt_start = std::chrono::high_resolution_clock::now();
-
-        if ((ret = writer->push_frame((uint8_t *)mem + i, chunk_size, 0)) != RTP_OK)
-            fprintf(stderr, "push_frame failed!\n");
-
-        fpt_end = std::chrono::high_resolution_clock::now();
-
-        i += chunk_size;
-        frames++;
-        fsize += chunk_size;
-        uint64_t diff = std::chrono::duration_cast<std::chrono::microseconds>(fpt_end - fpt_start).count();
-        fpt_ms += diff;
+    for (int i = 0; i < 1; ++i) {
+        auto t = new std::thread(runner, &rtp_ctx, i, mem, len);
+        t->detach();
     }
-    end = std::chrono::high_resolution_clock::now();
 
-    uint64_t diff = (uint64_t)std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-
-    fprintf(stderr, "%lu bytes, %lu kB, %lu MB took %u ms %u s\n",
-        fsize, fsize / 1000, fsize / 1000 / 1000,
-        diff, diff / 1000
-    );
-    fprintf(stderr, "# of frames: %u\n", frames);
-    fprintf(stderr, "avg frame size: %lu\n", fsize / frames);
-    fprintf(stderr, "avg processing time of frame: %lu\n", fpt_ms / frames);
-
-    rtp_ctx.destroy_writer(writer);
-    profiler::dumpBlocksToFile("/home/altonen/work/rtplib/benchmarks/receiving/profiler.prof");
+    for (;;)
+        std::this_thread::sleep_for(std::chrono::milliseconds(10000));
 }
--- benchmarks/sending/kvzrtp.cc
+++ benchmarks/sending/kvzrtp.cc
@@ -2,70 +2,91 @@
 #include <kvzrtp/clock.hh>
 #include <cstring>
 
-using std::chrono::high_resolution_clock;
+#include <easy/profiler.h>
 
 extern void *get_mem(int argc, char **argv, size_t& len);
 
-void recv_hook(void *arg, kvz_rtp::frame::rtp_frame *frame)
+size_t bytes_sent     = 0;
+size_t bytes_received = 0;
+
+void runner(kvz_rtp::context *rtp_ctx, int n, void *mem, size_t len)
 {
-    (void)kvz_rtp::frame::dealloc_frame(frame);
+#if 0
+    kvz_rtp::writer *writer = rtp_ctx->create_writer("10.21.25.26", 8888 + n, RTP_FORMAT_HEVC);
+#else
+    kvz_rtp::writer *writer = rtp_ctx->create_writer("127.0.0.1", 8888 + n, RTP_FORMAT_HEVC);
+#endif
+    (void)writer->start();
+
+    uint64_t chunk_size, total_size;
+    rtp_error_t ret;
+    uint64_t fpt_us = 0, diff = 0;
+    uint32_t frames = 0;
+    std::chrono::high_resolution_clock::time_point start, fpt_start, fpt_end, end;
+    start = std::chrono::high_resolution_clock::now();
+
+    for (int zzz = 0; zzz < 3; ++zzz) {
+        for (size_t i = 0; i < len; ) {
+            memcpy(&chunk_size, (uint8_t *)mem + i, sizeof(uint64_t));
+
+            i          += sizeof(uint64_t);
+            total_size += chunk_size;
+
+            fpt_start = std::chrono::high_resolution_clock::now();
+
+            if ((ret = writer->push_frame((uint8_t *)mem + i, chunk_size, 0)) != RTP_OK) {
+                fprintf(stderr, "push_frame failed!\n");
+                for (;;);
+            }
+
+            fpt_end = std::chrono::high_resolution_clock::now();
+            diff = std::chrono::duration_cast<std::chrono::milliseconds>(fpt_end - fpt_start).count();
+
+            fprintf(stderr, "sleep %ld\n", 8 - diff);
+            std::this_thread::sleep_for(std::chrono::milliseconds(8 - diff));
+            diff = std::chrono::duration_cast<std::chrono::microseconds>(fpt_end - fpt_start).count();
+
+            i += chunk_size;
+            frames++;
+            bytes_sent += chunk_size;
+            fpt_us += diff;
+        }
+    }
+
+    rtp_ctx->destroy_writer(writer);
+
+    end = std::chrono::high_resolution_clock::now();
+
+    diff = (uint64_t)std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+
+    fprintf(stderr, "%lu bytes, %lu kB, %lu MB took %lu ms %lu s\n",
+        bytes_sent, bytes_sent / 1000, bytes_sent / 1000 / 1000,
+        diff, diff / 1000
+    );
+    fprintf(stderr, "# of frames: %u\n", frames);
+    fprintf(stderr, "avg frame size: %lu\n", bytes_sent / frames);
+    fprintf(stderr, "avg processing time of frame: %lu us\n", fpt_us / frames);
+
+    /* profiler::dumpBlocksToFile("/home/altonen/work/rtplib/benchmarks/receiving/profiler.prof"); */
+
+    for (;;)
+        std::this_thread::sleep_for(std::chrono::milliseconds(10000));
 }
 
 int main(int argc, char **argv)
 {
+    /* EASY_PROFILER_ENABLE; */
+
     size_t len = 0;
     void *mem  = get_mem(argc, argv, len);
 
     kvz_rtp::context rtp_ctx;
 
-    kvz_rtp::writer *writer = rtp_ctx.create_writer("127.0.0.1", 8888, RTP_FORMAT_HEVC);
-    kvz_rtp::reader *reader  = rtp_ctx.create_reader("127.0.0.1", 8888, RTP_FORMAT_HEVC);
-
-    reader->install_recv_hook(NULL, recv_hook);
-
-    (void)reader->start();
-    (void)writer->start();
-
-    uint64_t chunk_size, total_size;
-    rtp_error_t ret;
-    uint64_t fpt_ms = 0;
-    uint64_t fsize  = 0;
-    uint32_t frames = 0;
-    uint64_t bytes  = 0;
-    std::chrono::high_resolution_clock::time_point start, fpt_start, fpt_end, end;
-    start = std::chrono::high_resolution_clock::now();
-
-    for (size_t i = 0; i < len; ) {
-        memcpy(&chunk_size, (uint8_t *)mem + i, sizeof(uint64_t));
-
-        i          += sizeof(uint64_t);
-        total_size += chunk_size;
-
-        fpt_start = std::chrono::high_resolution_clock::now();
-
-        if ((ret = writer->push_frame((uint8_t *)mem + i, chunk_size, 0)) != RTP_OK)
-            fprintf(stderr, "push_frame failed!\n");
-
-        fpt_end = std::chrono::high_resolution_clock::now();
-        std::this_thread::sleep_for(std::chrono::milliseconds(1));
-
-        i += chunk_size;
-        frames++;
-        fsize += chunk_size;
-        uint64_t diff = std::chrono::duration_cast<std::chrono::microseconds>(fpt_end - fpt_start).count();
-        fpt_ms += diff;
+    for (int i = 0; i < 1; ++i) {
+        auto t = new std::thread(runner, &rtp_ctx, i, mem, len);
+        t->detach();
     }
-    end = std::chrono::high_resolution_clock::now();
 
-    uint64_t diff = (uint64_t)std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-
-    fprintf(stderr, "%lu bytes, %lu kB, %lu MB took %u ms %u s\n",
-        fsize, fsize / 1000, fsize / 1000 / 1000,
-        diff, diff / 1000
-    );
-    fprintf(stderr, "# of frames: %u\n", frames);
-    fprintf(stderr, "avg frame size: %lu\n", fsize / frames);
-    fprintf(stderr, "avg processing time of frame: %lu\n", fpt_ms / frames);
-
-    rtp_ctx.destroy_writer(writer);
+    for (;;)
+        std::this_thread::sleep_for(std::chrono::milliseconds(10000));
 }
