0dc3709e4152432e3599d1f480193270883869dc
Aaro Altonen <aaro.altonen@tuni.fi>
2020-03-27 12:19:26 +0200

    Merge parse_kvzrtp_recv and parse_kvzrtp_send
    They're nearly identicaly, only one line difference, so it makes no
    sense to have to functions

--- benchmarks/parse.pl
+++ benchmarks/parse.pl
@@ -5,49 +5,8 @@ use strict;
 
 # extract average send/receive goodputs
 # and calculate averages for the time command outputs
-sub parse_kvzrtp_recv {
-	my ($path) = @_;
-
-	open(my $fh, '<', $path) or die "failed to open file";
-
-	my ($t_usr, $t_sys, $t_cpu, $t_total, $t_time, $t_bytes, $lines);
-
-	# each iteration parses one benchmark run
-	while (my $line = <$fh>) {
-		$line = <$fh>;
-		my ($bytes, $time) = ($line =~ m/(\d+)\s(\d+)/);
-
-		$line = <$fh>;
-		my ($usr, $sys, $total, $cpu) = ($line =~ m/(\d+\.\d+)user\s(\d+\.\d+)system\s0:(\d+.\d+)elapsed\s(\d+)%CPU/);
-
-		# discard line about inputs, outputs and pagefaults
-		$line = <$fh>;
-
-		# update total
-		$t_usr   += $usr;
-		$t_sys   += $sys;
-		$t_cpu   += $cpu;
-		$t_total += $total;
-		$t_time  += $time;
-		$t_bytes += $bytes;
-		$lines   += 1;
-	}
-
-	my $gp = int((($t_bytes / $lines) / 1000 / 1000 / ($t_time  / $lines)) * 1000);
-
-	print "$path: \n";
-	print "\tuser:    " .  $t_usr   / $lines . "\n";
-	print "\tsystem:  " .  $t_sys   / $lines . "\n";
-	print "\tcpu:     " .  $t_cpu   / $lines . "\n";
-	print "\ttotal:   " .  $t_total / $lines . "\n";
-	print "\ttime:    " .  $t_time  / $lines . " ms\n";
-	print "\tgoodput: " .  $gp . " MB/s\n";
-
-	close $fh;
-}
-
-sub parse_kvzrtp_send {
-	my ($path) = @_;
+sub parse_kvzrtp {
+	my ($role, $path) = @_;
 
 	open(my $fh, '<', $path) or die "failed to open file";
 
@@ -57,6 +16,13 @@ sub parse_kvzrtp_send {
 	while (my $line = <$fh>) {
 		$line = <$fh>;
 		my @nums = $line =~ /(\d+)/g;
+		$t_bytes += $nums[0];
+
+		if ($role eq  "recv") {
+			$t_time  += $nums[1];
+		} else {
+			$t_time  += $nums[3];
+		}
 
 		$line = <$fh>;
 		my ($usr, $sys, $total, $cpu) = ($line =~ m/(\d+\.\d+)user\s(\d+\.\d+)system\s0:(\d+.\d+)elapsed\s(\d+)%CPU/);
@@ -69,8 +35,6 @@ sub parse_kvzrtp_send {
 		$t_sys   += $sys;
 		$t_cpu   += $cpu;
 		$t_total += $total;
-		$t_bytes += $nums[0];
-		$t_time  += $nums[3];
 		$lines   += 1;
 	}
 
@@ -97,11 +61,7 @@ if ($#ARGV + 1 != 3) {
 my ($lib, $role, $path) = @ARGV;
 
 if ($lib eq "kvzrtp") {
-	if ($role eq "send") {
-		parse_kvzrtp_send($path);
-	} else {
-		parse_kvzrtp_recv($path);
-	}
+	parse_kvzrtp($role, $path);
 } elsif ($lib eq "ffmpeg") {
 	die "not implemented";
 } elsif ($lib eq "gstreamer") {
