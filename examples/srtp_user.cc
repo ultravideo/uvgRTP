3525ee0e3e1463ccdd52e268c93d8030c5abf201
Aaro Altonen <aaro.altonen@tuni.fi>
2020-04-30 01:46:55 +0300

    Fix SRTP example with user-managed keys

--- examples/srtp_user.cc
+++ examples/srtp_user.cc
@@ -3,9 +3,13 @@
 
 #define PAYLOAD_MAXLEN  256
 #define KEY_SIZE         16
+#define SALT_SIZE        14
 
-/* Key for the SRTP session of sender and receiver */
-uint8_t key[KEY_SIZE] = { 0 };
+/* Key and salt for the SRTP session of sender and receiver
+ *
+ * NOTE: uvgRTP only supports 128 bit keys and 112 bit salts */
+uint8_t key[KEY_SIZE]   = { 0 };
+uint8_t salt[SALT_SIZE] = { 0 };
 
 void thread_func(void)
 {
@@ -13,22 +17,33 @@ void thread_func(void)
     uvg_rtp::context ctx;
     uvg_rtp::session *sess = ctx.create_session("127.0.0.1");
 
-    /* Enable SRTP and use ZRTP to manage keys */
+    /* Enable SRTP and let user manage keys */
     unsigned flags = RCE_SRTP | RCE_SRTP_KMNGMNT_USER;
 
     /* See sending.cc for more details about create_stream() */
     uvg_rtp::media_stream *recv = sess->create_stream(8889, 8888, RTP_FORMAT_GENERIC, flags);
 
-    send->srtp_add_key(key, KEY_SIZE);
+    /* Before anything else can be done,
+     * add_srtp_ctx() must be called with the SRTP key and salt.
+     *
+     * All calls to "recv" that try to modify and or/use the newly
+     * created media stream before calling add_srtp_ctx() will fail */
+    recv->add_srtp_ctx(key, salt);
 
-    /* All media is now encrypted/decrypted automatically */
+    for (;;) {
+        auto frame = recv->pull_frame();
+        fprintf(stderr, "Message: '%s'\n", frame->payload);
+    }
 }
 
 int main(void)
 {
-    /* initialize SRTP key */
+    /* initialize SRTP key and salt */
     for (int i = 0; i < KEY_SIZE; ++i)
         key[i] = i;
+
+    for (int i = 0; i < SALT_SIZE; ++i)
+        salt[i] = i * 2;
 
     /* Create separate thread for the receiver */
     new std::thread(thread_func);
@@ -37,13 +52,25 @@ int main(void)
     uvg_rtp::context ctx;
     uvg_rtp::session *sess = ctx.create_session("127.0.0.1");
 
-    /* Enable SRTP and use ZRTP to manage keys */
+    /* Enable SRTP and let user manage keys */
     unsigned flags = RCE_SRTP | RCE_SRTP_KMNGMNT_USER;
 
     /* See sending.cc for more details about create_stream() */
     uvg_rtp::media_stream *send = sess->create_stream(8888, 8889, RTP_FORMAT_GENERIC, flags);
 
-    send->srtp_add_key(key, KEY_SIZE);
+    /* Before anything else can be done,
+     * add_srtp_ctx() must be called with the SRTP key and salt.
+     *
+     * All calls to "send" that try to modify and or/use the newly
+     * created media stream before calling add_srtp_ctx() will fail */
+    send->add_srtp_ctx(key, salt);
 
     /* All media is now encrypted/decrypted automatically */
+    char *message  = (char *)"Hello, world!";
+    size_t msg_len = strlen(message);
+
+    for (;;) {
+        send->push_frame((uint8_t *)message, msg_len, RTP_NO_FLAGS);
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
 }
