f104127d0f2a9cdd06e603b43bcc7792cb4e7f47
Aaro Altonen <aaro.altonen@tuni.fi>
2020-05-01 18:29:16 +0300

    Convert tabs to spaces

--- benchmarks/benchmark.pl
+++ benchmarks/benchmark.pl
@@ -9,67 +9,67 @@ use Getopt::Long;
 $| = 1; # autoflush
 
 sub clamp {
-	my ($start, $end) = @_;
-	my @clamped = (0, 0);
+    my ($start, $end) = @_;
+    my @clamped = (0, 0);
 
-	$clamped[0] = $start < 15   ? 15   : $start;
-	$clamped[1] = $end   > 1500 ? 1500 : $end;
+    $clamped[0] = $start < 15   ? 15   : $start;
+    $clamped[1] = $end   > 1500 ? 1500 : $end;
 
-	return @clamped;
+    return @clamped;
 }
 
 sub send_benchmark {
-	my ($lib, $addr, $port, $iter, $threads, $start, $end, $gen_recv) = @_;
-	my ($sfps, $efps) = clamp($start, $end);
-	my ($socket, $remote, $data);
+    my ($lib, $addr, $port, $iter, $threads, $start, $end, $gen_recv) = @_;
+    my ($sfps, $efps) = clamp($start, $end);
+    my ($socket, $remote, $data);
 
-	$socket = IO::Socket::INET->new(
-		LocalAddr => $addr,
-		LocalPort => $port,
-		Proto     => "tcp",
-		Type      => SOCK_STREAM,
-		Listen    => 1,
-	) or die "Couldn't connect to $addr:$port : $@\n";
+    $socket = IO::Socket::INET->new(
+        LocalAddr => $addr,
+        LocalPort => $port,
+        Proto     => "tcp",
+        Type      => SOCK_STREAM,
+        Listen    => 1,
+    ) or die "Couldn't connect to $addr:$port : $@\n";
 
-	$remote = $socket->accept();
+    $remote = $socket->accept();
 
-	while ($threads ne 0) {
-		for (my $i = $sfps; $i <= $efps; $i *= 2) {
-			my $logname = "send_results_$threads" . "threads_$i". "fps_$iter" . "iter";
-			for ((1 .. $iter)) {
-				$remote->recv($data, 16);
-				system ("time ./$lib/sender $addr $threads $i >> $lib/results/$logname 2>&1");
+    while ($threads ne 0) {
+        for (my $i = $sfps; $i <= $efps; $i *= 2) {
+            my $logname = "send_results_$threads" . "threads_$i". "fps_$iter" . "iter";
+            for ((1 .. $iter)) {
+                $remote->recv($data, 16);
+                system ("time ./$lib/sender $addr $threads $i >> $lib/results/$logname 2>&1");
                 $remote->send("end") if $gen_recv;
-			}
-		}
+            }
+        }
 
-		$threads--;
-	}
+        $threads--;
+    }
 }
 
 sub recv_benchmark {
-	my ($lib, $addr, $port, $iter, $threads, $start, $end) = @_;
-	my ($sfps, $efps) = clamp($start, $end);
+    my ($lib, $addr, $port, $iter, $threads, $start, $end) = @_;
+    my ($sfps, $efps) = clamp($start, $end);
 
-	my $socket = IO::Socket::INET->new(
-		PeerAddr  => $addr,
-		PeerPort  => $port,
-		Proto     => "tcp",
-		Type      => SOCK_STREAM,
-		Timeout   => 1,
-	) or die "Couldn't connect to $addr:$port : $@\n";
+    my $socket = IO::Socket::INET->new(
+        PeerAddr  => $addr,
+        PeerPort  => $port,
+        Proto     => "tcp",
+        Type      => SOCK_STREAM,
+        Timeout   => 1,
+    ) or die "Couldn't connect to $addr:$port : $@\n";
 
-	while ($threads ne 0) {
-		for (my $i = $sfps; $i <= $efps; $i *= 2) {
-			my $logname = "recv_results_$threads" . "threads_$i". "fps_$iter" . "iter";
-			for ((1 .. $iter)) {
-				$socket->send("start");
-				system ("time ./$lib/receiver $addr $threads >> $lib/results/$logname 2>&1");
-			}
-		}
+    while ($threads ne 0) {
+        for (my $i = $sfps; $i <= $efps; $i *= 2) {
+            my $logname = "recv_results_$threads" . "threads_$i". "fps_$iter" . "iter";
+            for ((1 .. $iter)) {
+                $socket->send("start");
+                system ("time ./$lib/receiver $addr $threads >> $lib/results/$logname 2>&1");
+            }
+        }
 
-		$threads--;
-	}
+        $threads--;
+    }
 }
 
 # use netcat to capture the stream
@@ -116,32 +116,32 @@ sub recv_generic {
 }
 
 GetOptions(
-	"lib=s"     => \(my $lib = ""),
-	"role=s"    => \(my $role = ""),
-	"addr=s"    => \(my $addr = ""),
-	"port=i"    => \(my $port = 0),
-	"iter=i"    => \(my $iter = 10),
-	"threads=i" => \(my $threads = 1),
-	"start=f"   => \(my $start = 0),
-	"end=f"     => \(my $end = 0),
+    "lib=s"     => \(my $lib = ""),
+    "role=s"    => \(my $role = ""),
+    "addr=s"    => \(my $addr = ""),
+    "port=i"    => \(my $port = 0),
+    "iter=i"    => \(my $iter = 10),
+    "threads=i" => \(my $threads = 1),
+    "start=f"   => \(my $start = 0),
+    "end=f"     => \(my $end = 0),
     "use-nc"    => \(my $nc = 0)
 ) or die "failed to parse command line!\n";
 
 if ($lib eq "") {
-	print "library not defined!\n" and exit;
+    print "library not defined!\n" and exit;
 }
 
 if (!$start or !$end) {
-	print "start and end FPS values must be defined!\n" and exit;
+    print "start and end FPS values must be defined!\n" and exit;
 }
 
 if ($addr eq "" or $port eq 0) {
-	print "address and port must be defined!\n" and exit;
+    print "address and port must be defined!\n" and exit;
 }
 
 if ($role eq "send") {
-	system ("make $lib" . "_sender");
-	send_benchmark($lib, $addr, $port, $iter, $threads, $start, $end, $nc);
+    system ("make $lib" . "_sender");
+    send_benchmark($lib, $addr, $port, $iter, $threads, $start, $end, $nc);
 } elsif ($role eq "recv" ) {
     if (!$nc) {
         system ("make $lib" . "_receiver");
@@ -150,5 +150,5 @@ if ($role eq "send") {
         recv_generic($lib, $addr, $port, $iter, $threads, $start, $end);
     }
 } else {
-	print "invalid role: '$role'\n" and exit;
+    print "invalid role: '$role'\n" and exit;
 }
