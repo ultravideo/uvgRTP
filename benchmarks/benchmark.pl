a273763060b04a4f46107def365714a4808054aa
Aaro Altonen <aaro.altonen@tuni.fi>
2020-05-02 10:59:38 +0300

    Add code for testing latency

--- benchmarks/benchmark.pl
+++ benchmarks/benchmark.pl
@@ -18,19 +18,36 @@ sub clamp {
     return @clamped;
 }
 
+sub mk_ssock {
+    my $s = IO::Socket::INET->new(
+        LocalAddr => $_[0],
+        LocalPort => $_[1],
+        Proto     => "tcp",
+        Type      => SOCK_STREAM,
+        Listen    => 1,
+    ) or die "Couldn't connect to $_[0]:$_[1]: $@\n";
+
+    return $s;
+}
+
+sub mk_rsock {
+    my $s = IO::Socket::INET->new(
+        PeerAddr  => $_[0],
+        PeerPort  => $_[1],
+        Proto     => "tcp",
+        Type      => SOCK_STREAM,
+        Timeout   => 1,
+    ) or die "Couldn't connect to $_[0]:$_[1]: $@\n";
+
+    return $s;
+}
+
 sub send_benchmark {
     my ($lib, $addr, $port, $iter, $threads, $start, $end, $gen_recv) = @_;
     my ($sfps, $efps) = clamp($start, $end);
     my ($socket, $remote, $data);
 
-    $socket = IO::Socket::INET->new(
-        LocalAddr => $addr,
-        LocalPort => $port,
-        Proto     => "tcp",
-        Type      => SOCK_STREAM,
-        Listen    => 1,
-    ) or die "Couldn't connect to $addr:$port : $@\n";
-
+    $socket = mk_ssock($addr, $port);
     $remote = $socket->accept();
 
     while ($threads ne 0) {
@@ -50,14 +67,7 @@ sub send_benchmark {
 sub recv_benchmark {
     my ($lib, $addr, $port, $iter, $threads, $start, $end) = @_;
     my ($sfps, $efps) = clamp($start, $end);
-
-    my $socket = IO::Socket::INET->new(
-        PeerAddr  => $addr,
-        PeerPort  => $port,
-        Proto     => "tcp",
-        Type      => SOCK_STREAM,
-        Timeout   => 1,
-    ) or die "Couldn't connect to $addr:$port : $@\n";
+    my $socket = mk_rsock($addr, $port);
 
     while ($threads ne 0) {
         for (my $i = $sfps; $i <= $efps; $i *= 2) {
@@ -76,15 +86,8 @@ sub recv_benchmark {
 sub recv_generic {
     my ($lib, $addr, $port, $iter, $threads, $start, $end) = @_;
     my ($sfps, $efps) = clamp($start, $end);
+    my $socket = mk_rsock($addr, $port);
     my $ports = "";
-
-    my $socket = IO::Socket::INET->new(
-        PeerAddr  => $addr,
-        PeerPort  => $port,
-        Proto     => "tcp",
-        Type      => SOCK_STREAM,
-        Timeout   => 1,
-    ) or die "Couldn't connect to $addr:$port : $@\n";
 
     # spawn N netcats using gnu parallel, send message to sender to start sending,
     # wait for message from sender that all the packets have been sent, sleep a tiny bit
@@ -115,6 +118,26 @@ sub recv_generic {
     }
 }
 
+sub lat_send {
+    my ($lib, $addr, $port, $role) = @_;
+    my ($socket, $remote, $data);
+
+    $socket = mk_ssock($addr, $port);
+    $remote = $socket->accept();
+
+    $socket->recv($data, 16);
+    system "$lib/latency $role >> $lib/results/latency_results 2>&1";
+}
+
+sub lat_recv {
+    my ($lib, $addr, $port, $role) = @_;
+    my $socket = mk_rsock($addr, $port);
+    my $data;
+
+    $socket->send("start");
+    system "$lib/latency $role >/dev/null 2>&1";
+}
+
 sub print_help {
     print "usage (benchmark):\n  ./benchmark.pl \n"
     . "\t--lib <uvgrtp|ffmpeg|gstreamer>\n"
@@ -123,7 +146,14 @@ sub print_help {
     . "\t--port <server port>\n"
     . "\t--threads <# of threads>\n"
     . "\t--start <start fps>\n"
-    . "\t--end <end fps>\n" and exit;
+    . "\t--end <end fps>\n\n";
+
+    print "usage (latency):\n  ./benchmark.pl \n"
+    . "\t--latency\n"
+    . "\t--role <send|recv>\n"
+    . "\t--addr <server address>\n"
+    . "\t--port <server port>\n"
+    . "\t--lib <uvgrtp|ffmpeg|gstreamer>\n\n" and exit;
 }
     
 GetOptions(
@@ -136,19 +166,30 @@ GetOptions(
     "start|s=f"   => \(my $start = 0),
     "end|e=f"     => \(my $end = 0),
     "use-nc"      => \(my $nc = 0),
+    "latency"     => \(my $lat = 0),
     "help"        => \(my $help = 0)
 ) or die "failed to parse command line!\n";
 
 print_help() if $help;
 print_help() if !$lib or !$addr or !$port;
-print_help() if !$start or !$end;
+print_help() if (!$start or !$end) and !$lat;
+
+die "not implemented\n" if !grep (/$lib/, ("uvgrtp", "ffmpeg"));
 
 if ($role eq "send") {
-    system ("make $lib" . "_sender");
-    send_benchmark($lib, $addr, $port, $iter, $threads, $start, $end, $nc);
+    if ($lat) {
+        system "make $lib" . "_latency";
+        lat_send($lib, $addr, $port, $role);
+    } else {
+        system "make $lib" . "_sender";
+        send_benchmark($lib, $addr, $port, $iter, $threads, $start, $end, $nc);
+    }
 } elsif ($role eq "recv" ) {
-    if (!$nc) {
-        system ("make $lib" . "_receiver");
+    if ($lat) {
+        system "make $lib" . "_latency";
+        lat_recv($lib, $addr, $port, $role);
+    } elsif (!$nc) {
+        system "make $lib" . "_receiver";
         recv_benchmark($lib, $addr, $port, $iter, $threads, $start, $end);
     } else {
         recv_generic($lib, $addr, $port, $iter, $threads, $start, $end);
