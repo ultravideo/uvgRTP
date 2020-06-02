a8192946450bc42d9196da5975a5ae43c7e73c0e
Aaro Altonen <aaro.altonen@tuni.fi>
2020-06-02 08:41:32 +0300

    Fix default values for Live555's receiver

--- benchmarks/live555/receiver.cc
+++ benchmarks/live555/receiver.cc
@@ -12,10 +12,10 @@ int main(int argc, char **argv)
 
     Port rtpPort(8888);
     struct in_addr dst_addr;
-    dst_addr.s_addr = our_inet_addr("127.0.0.1");
+    dst_addr.s_addr = our_inet_addr("0.0.0.0");
     Groupsock rtpGroupsock(*env, dst_addr, rtpPort, 255);
 
-    OutPacketBuffer::maxSize = 1600000;
+    OutPacketBuffer::maxSize = 40 * 1000 * 1000;
 
     RTPSource *source = H265VideoRTPSource::createNew(*env, &rtpGroupsock, 96);
     RTPSink_ *sink    = new RTPSink_(*env);
