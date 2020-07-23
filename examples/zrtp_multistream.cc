3eaa6cb05668daad8d23d759b89789562605a2be
Aaro Altonen <aaro.altonen@tuni.fi>
2020-07-23 12:55:24 +0300

    Add example code for ZRTP multistream mode

--- examples/zrtp_multistream.cc
+++ examples/zrtp_multistream.cc
@@ -11,14 +11,19 @@ void thread_func(void)
     /* Enable SRTP and use ZRTP to manage keys */
     unsigned flags = RCE_SRTP | RCE_SRTP_KMNGMNT_ZRTP;
 
-    /* See sending.cc for more details about create_stream() */
-    uvg_rtp::media_stream *recv = sess->create_stream(8889, 8888, RTP_FORMAT_GENERIC, flags);
+    /* Keys creates using Diffie-Hellman mode */
+    uvg_rtp::media_stream *recv1 = sess->create_stream(8889, 8888, RTP_FORMAT_GENERIC, flags);
+
+    /* Keys created using Multistream mode */
+    uvg_rtp::media_stream *recv2 = sess->create_stream(7778, 7777, RTP_FORMAT_GENERIC, flags);
 
     for (;;) {
-        auto frame = recv->pull_frame();
+        auto frame = recv1->pull_frame();
         fprintf(stderr, "Message: '%s'\n", frame->payload);
+        (void)uvg_rtp::frame::dealloc_frame(frame);
 
-        /* the frame must be destroyed manually */
+        frame = recv2->pull_frame();
+        fprintf(stderr, "Message: '%s'\n", frame->payload);
         (void)uvg_rtp::frame::dealloc_frame(frame);
     }
 }
@@ -39,14 +44,20 @@ int main(void)
     /* Enable SRTP and use ZRTP to manage keys */
     unsigned flags = RCE_SRTP | RCE_SRTP_KMNGMNT_ZRTP;
 
-    /* See sending.cc for more details about create_stream() */
-    uvg_rtp::media_stream *send = sess->create_stream(8888, 8889, RTP_FORMAT_GENERIC, flags);
+    /* Initialize ZRTP and negotiate the keys used to encrypt the media */
+    uvg_rtp::media_stream *send1 = sess->create_stream(8888, 8889, RTP_FORMAT_GENERIC, flags);
+
+    /* The first call to create_stream() creates keys for the session using Diffie-Hellman
+     * key exchange and all subsequent calls to create_stream() initialize keys for the
+     * stream using Multistream mode */
+    uvg_rtp::media_stream *send2 = sess->create_stream(7777, 7778, RTP_FORMAT_GENERIC, flags);
 
     char *message  = (char *)"Hello, world!";
     size_t msg_len = strlen(message);
 
     for (;;) {
-        send->push_frame((uint8_t *)message, msg_len, RTP_NO_FLAGS);
+        send1->push_frame((uint8_t *)message, msg_len, RTP_NO_FLAGS);
+        send2->push_frame((uint8_t *)message, msg_len, RTP_NO_FLAGS);
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
     }
 }
