fc597fd2f80d7e1db3c3ce8728f8eb20d16bd6ab
Aaro Altonen <aaro.altonen@tuni.fi>
2020-08-03 10:58:50 +0300

    Add receive hook to RTP packet dispatcher

--- include/pkt_dispatch.hh
+++ include/pkt_dispatch.hh
@@ -21,6 +21,12 @@ namespace uvg_rtp {
              * Return RTP_INVALID_VALUE if "handler" is nullptr */
             rtp_error_t install_handler(packet_handler handler);
 
+            /* Install receive hook for the RTP packet dispatcher
+             *
+             * Return RTP_OK on success
+             * Return RTP_INVALID_VALUE if "hook" is nullptr */
+            rtp_error_t install_receive_hook(void *arg, void (*hook)(void *, uvg_rtp::frame::rtp_frame *));
+
             /* Return reference to the vector that holds all installed handlers */
             std::vector<uvg_rtp::packet_handler>& get_handlers();
 
@@ -29,5 +35,8 @@ namespace uvg_rtp {
 
             uvg_rtp::socket socket_;
             std::vector<packet_handler> packet_handlers_;
+
+            void *recv_hook_arg_;
+            void (*recv_hook_)(void *arg, uvg_rtp::frame::rtp_frame *frame);
     };
 }
