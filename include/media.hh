3519378821af97b8f798e2348f7af227bfc1ef82
Aaro Altonen <aaro.altonen@tuni.fi>
2020-08-03 10:10:29 +0300

    Introduce Media object
    Media is the base class from which all other media formats inherit
    the common operations.
    
    Media object contains two push_frame()s: one for normal pointers and
    one for smart pointers. It also contains a packet handler which is
    called by the RTP packet dispatcher when a UDP datagram is received.
    
    All media formats must implement packet_handler() and __push_frame()
    methods. If the media media format does not require any extra processing
    (such as Opus), it can directly use the Media object and its implementation
    of __push_frame() and packet_handler().
    
    Media object implements the generic interface which supports sending
    any kind of data and a uvgRTP-specific way of fragmenting/reconstructing
    generic RTP frames which can be enabled with RCE_FRAGMENT_GENERIC.

---
+++ include/media.hh
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <memory>
+
+#include "rtp.hh"
+#include "socket.hh"
+#include "util.hh"
+
+namespace uvg_rtp {
+
+    namespace formats {
+
+        class media {
+            public:
+                media(uvg_rtp::socket *socket, uvg_rtp::rtp *rtp_ctx, int flags);
+                virtual ~media();
+
+                /* These two functions are called by media_stream which is self is called by the application.
+                 * They act as thunks and forward the call to __push_frame() which every media should
+                 * implement if they require more processing than what the default implementation offers
+                 *
+                 * Return RTP_OK on success */
+                rtp_error_t push_frame(uint8_t *data, size_t data_len, int flags);
+                rtp_error_t push_frame(std::unique_ptr<uint8_t[]> data, size_t data_len, int flags);
+
+                /* Media-specific packet handler. The default handler, depending on what "flags_" contains,
+                 * may only return the received RTP packet or it may merge multiple packets together before
+                 * returning a complete frame to the user.
+                 *
+                 * If the implemented media requires more fine-tuned processing, this should be overrode
+                 *
+                 * Return RTP_OK if the packet was successfully handled
+                 * Return RTP_PKT_NOT_HANDLED if the packet is not handled by this handler
+                 * Return RTP_PKT_MODIFIED if the packet was modified but should be forwarded to other handlers
+                 * Return RTP_GENERIC_ERROR if the packet was corrupted in some way */
+                static rtp_error_t packet_handler(ssize_t size, void *packet);
+
+            protected:
+                virtual rtp_error_t __push_frame(uint8_t *data, size_t data_len, int flags);
+
+                uvg_rtp::socket *socket_;
+                uvg_rtp::rtp *rtp_ctx_;
+                int flags_;
+        };
+    };
+};
