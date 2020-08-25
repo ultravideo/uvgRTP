19ba6e1c5c808773729dde11546f9c6d31091bf8
Aaro Altonen <aaro.altonen@tuni.fi>
2020-08-25 11:09:31 +0300

    Deprecate RTCP Receive Report support from src/frame.cc
    The frame (de)allocation is handled internally by each message
    handler and include/frame.hh shall only provide the structure
    format for RTCP Receive Report.

--- src/frame.cc
+++ src/frame.cc
@@ -134,39 +134,6 @@ uvg_rtp::frame::rtcp_sender_frame *uvg_rtp::frame::alloc_rtcp_sender_frame(size_
     return frame;
 }
 
-uvg_rtp::frame::rtcp_receiver_frame *uvg_rtp::frame::alloc_rtcp_receiver_frame(size_t nblocks)
-{
-    if (nblocks == 0) {
-        LOG_ERROR("Cannot send 0 report blocks!");
-        rtp_errno = RTP_INVALID_VALUE;
-        return nullptr;
-    }
-
-    size_t total_size =
-        sizeof(rtcp_header) +
-        sizeof(uint32_t) +
-        sizeof(rtcp_report_block) * nblocks;
-
-    auto *frame = (uvg_rtp::frame::rtcp_receiver_frame *)new uint8_t[total_size];
-
-    if (!frame) {
-        LOG_ERROR("Failed to allocate memory for RTCP sender report");
-        rtp_errno = RTP_MEMORY_ERROR;
-        return nullptr;
-    }
-
-    frame->header.version  = 2;
-    frame->header.padding  = 0;
-    frame->header.pkt_type = uvg_rtp::frame::RTCP_FT_RR;
-    frame->header.length   = total_size;
-    frame->header.count    = nblocks;
-
-    /* caller fills these */
-    memset(frame->blocks, 0, sizeof(rtcp_report_block) * nblocks);
-
-    return frame;
-}
-
 uvg_rtp::frame::rtcp_sdes_frame *uvg_rtp::frame::alloc_rtcp_sdes_frame(size_t ssrc_count, size_t total_len)
 {
     if (total_len == 0) {
@@ -249,15 +216,6 @@ uvg_rtp::frame::rtcp_app_frame *uvg_rtp::frame::alloc_rtcp_app_frame(std::string
 }
 
 rtp_error_t uvg_rtp::frame::dealloc_frame(uvg_rtp::frame::rtcp_sender_frame *frame)
-{
-    if (!frame)
-        return RTP_INVALID_VALUE;
-
-    delete[] frame;
-    return RTP_OK;
-}
-
-rtp_error_t uvg_rtp::frame::dealloc_frame(uvg_rtp::frame::rtcp_receiver_frame *frame)
 {
     if (!frame)
         return RTP_INVALID_VALUE;
