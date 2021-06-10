62370c4d96ce9f840e8c81441df62a6635dcf6e6
Joni Räsänen <joni.rasanen@iki.fi>
2021-06-10 15:35:48 +0300

    zrtp: Calculate length field correctly

--- src/zrtp/zrtp_message.cc
+++ src/zrtp/zrtp_message.cc
@@ -5,6 +5,8 @@
 
 #include "debug.hh"
 
+#include <string>
+
 uvgrtp::zrtp_msg::zrtp_message::zrtp_message():
     frame_(nullptr),
     rframe_(nullptr),
@@ -54,11 +56,20 @@ void uvgrtp::zrtp_msg::zrtp_message::set_zrtp_start_base(uvgrtp::zrtp_msg::zrtp_
     start.header.version = 0;
     start.header.magic = ZRTP_HEADER_MAGIC;
     start.magic = ZRTP_MSG_MAGIC;
-    start.length = (uint16_t)len_ - (uint16_t)sizeof(zrtp_header);
 
+    uint16_t length = (uint16_t)len_ - (uint16_t)sizeof(zrtp_header);
+
+    if (length%4 != 0)
+    {
+      std::string printText = "ZRTP message length is not divisible by 32-bit word. Type: " + msgblock;
+      log_platform_error(printText.c_str());
+    }
+
+    // convert size into 32-bit words and minus the size of crc
+    start.length = length/4 - 1;
     memcpy(&start.msgblock, msgblock.c_str(), 8);
 
-    LOG_DEBUG("Constructed ZRTP header. Type: " + msgblock.c_str() + " Length: %u", start.length);
+    LOG_DEBUG("Constructed ZRTP header. Length: %u", start.length);
 }
 
 void uvgrtp::zrtp_msg::zrtp_message::set_zrtp_start(uvgrtp::zrtp_msg::zrtp_msg& start,
