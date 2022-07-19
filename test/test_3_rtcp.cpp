da9c1bbbbb7c5ae5af489f68e370ae73bcd55923
Joni Räsänen <joni.rasanen@iki.fi>
2022-07-19 11:21:33 +0300

    tests: Add test for APP packet

--- test/test_3_rtcp.cpp
+++ test/test_3_rtcp.cpp
@@ -15,6 +15,7 @@ constexpr int PACKET_INTERVAL_MS = 1000 / FRAME_RATE;
 void receiver_hook(uvgrtp::frame::rtcp_receiver_report* frame);
 void sender_hook(uvgrtp::frame::rtcp_sender_report* frame);
 void sdes_hook(uvgrtp::frame::rtcp_sdes_packet* frame);
+void app_hook(uvgrtp::frame::rtcp_app_packet* frame);
 void cleanup(uvgrtp::context& ctx, uvgrtp::session* local_session, uvgrtp::session* remote_session,
     uvgrtp::media_stream* send, uvgrtp::media_stream* receive);
 
@@ -53,6 +54,54 @@ TEST(RTCPTests, rtcp) {
         EXPECT_EQ(RTP_OK, remote_stream->get_rtcp()->install_sender_hook(sender_hook));
         EXPECT_EQ(RTP_OK, remote_stream->get_rtcp()->install_sdes_hook(sdes_hook));
     }
+
+    std::unique_ptr<uint8_t[]> test_frame = std::unique_ptr<uint8_t[]>(new uint8_t[PAYLOAD_LEN]);
+    memset(test_frame.get(), 'b', PAYLOAD_LEN);
+    send_packets(std::move(test_frame), PAYLOAD_LEN, local_session, local_stream, SEND_TEST_PACKETS, PACKET_INTERVAL_MS, true, RTP_NO_FLAGS);
+
+    cleanup(ctx, local_session, remote_session, local_stream, remote_stream);
+}
+
+TEST(RTCPTests, rtcp_app) {
+    std::cout << "Starting uvgRTP RTCP tests" << std::endl;
+
+    // Creation of RTP stream. See sending example for more details
+    uvgrtp::context ctx;
+    uvgrtp::session* local_session = ctx.create_session(REMOTE_ADDRESS);
+    uvgrtp::session* remote_session = ctx.create_session(LOCAL_INTERFACE);
+
+    int flags = RCE_RTCP;
+
+    uvgrtp::media_stream* local_stream = nullptr;
+    if (local_session)
+    {
+        local_stream = local_session->create_stream(LOCAL_PORT, REMOTE_PORT, RTP_FORMAT_GENERIC, flags);
+    }
+
+    uvgrtp::media_stream* remote_stream = nullptr;
+    if (remote_session)
+    {
+        remote_stream = remote_session->create_stream(REMOTE_PORT, LOCAL_PORT, RTP_FORMAT_GENERIC, flags);
+    }
+
+    EXPECT_NE(nullptr, remote_stream);
+
+    if (local_stream)
+    {
+        EXPECT_EQ(RTP_OK, local_stream->get_rtcp()->install_receiver_hook(receiver_hook));
+        EXPECT_EQ(RTP_OK, local_stream->get_rtcp()->install_sdes_hook(sdes_hook));
+        EXPECT_EQ(RTP_OK, local_stream->get_rtcp()->install_app_hook(app_hook));
+    }
+
+    if (remote_stream)
+    {
+        EXPECT_EQ(RTP_OK, remote_stream->get_rtcp()->install_sender_hook(sender_hook));
+        EXPECT_EQ(RTP_OK, remote_stream->get_rtcp()->install_sdes_hook(sdes_hook));
+        EXPECT_EQ(RTP_OK, remote_stream->get_rtcp()->install_app_hook(app_hook));
+    }
+    const char* data = "ABCD";
+    local_stream->get_rtcp()->send_app_packet("Test", 1, 4, (uint8_t*)data);
+    remote_stream->get_rtcp()->send_app_packet("Test", 1, 4, (uint8_t*)data);
 
     std::unique_ptr<uint8_t[]> test_frame = std::unique_ptr<uint8_t[]>(new uint8_t[PAYLOAD_LEN]);
     memset(test_frame.get(), 'b', PAYLOAD_LEN);
@@ -199,6 +248,12 @@ void sdes_hook(uvgrtp::frame::rtcp_sdes_packet* frame)
     std::cout << "Got SDES frame with " << frame->chunks.size() << " chunk" << std::endl;
 
     /* RTCP frames can be deallocated using delete */
+    delete frame;
+}
+
+void app_hook(uvgrtp::frame::rtcp_app_packet* frame)
+{
+    std::cout << "Got APP frame with " << frame->header.length << " 32-bit length" << std::endl;
     delete frame;
 }
 
