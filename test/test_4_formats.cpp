ae8a6472277ac89c817f84769828fb339899e5e0
Joni Räsänen <joni.rasanen@iki.fi>
2022-08-22 12:51:37 +0300

    tests: Remove non fps large frame test

--- test/test_4_formats.cpp
+++ test/test_4_formats.cpp
@@ -133,45 +133,6 @@ TEST(FormatTests, h265)
     cleanup_sess(ctx, sess);
 }
 
-TEST(FormatTests, h265_large)
-{
-    std::cout << "Starting h265 test" << std::endl;
-    uvgrtp::context ctx;
-    uvgrtp::session* sess = ctx.create_session(LOCAL_ADDRESS);
-
-    uvgrtp::media_stream* sender = nullptr;
-    uvgrtp::media_stream* receiver = nullptr;
-
-    if (sess)
-    {
-        sender = sess->create_stream(SEND_PORT, RECEIVE_PORT, RTP_FORMAT_H265, RCE_NO_FLAGS);
-        receiver = sess->create_stream(RECEIVE_PORT, SEND_PORT, RTP_FORMAT_H265, RCE_H26X_PREPEND_SC);
-        
-        if (receiver)
-        {
-            receiver->configure_ctx(RCC_UDP_RCV_BUF_SIZE, 40 * 1000 * 1000);
-        }
-    }
-
-    std::vector<size_t> test_sizes = {100000, 200000, 300000, 400000, 500000, 750000, 1000000};
-
-    // the default packet limit for RTP is 1458 where 12 bytes are dedicated to RTP header
-    int rtp_flags = RTP_NO_FLAGS;
-    int nal_type = 5;
-    rtp_format_t format = RTP_FORMAT_H265;
-    int test_runs = 10;
-
-    for (auto& size : test_sizes)
-    {
-        std::unique_ptr<uint8_t[]> intra_frame = create_test_packet(format, nal_type, true, size, rtp_flags);
-        test_packet_size(std::move(intra_frame), test_runs, size, sess, sender, receiver, rtp_flags);
-    }
-
-    cleanup_ms(sess, sender);
-    cleanup_ms(sess, receiver);
-    cleanup_sess(ctx, sess);
-}
-
 TEST(FormatTests, h265_large_fps)
 {
     std::cout << "Starting h265 test" << std::endl;
@@ -188,8 +149,8 @@ TEST(FormatTests, h265_large_fps)
 
         if (receiver)
         {
-            sender->configure_ctx(RCC_FPS_ENUM, 100);
-            sender->configure_ctx(RCC_FPS_DENUM, 1);
+            sender->configure_ctx(RCC_FPS_ENUMERATOR, 100);
+            sender->configure_ctx(RCC_FPS_DENOMINATOR, 1);
 
             receiver->configure_ctx(RCC_UDP_RCV_BUF_SIZE, 40 * 1000 * 1000);
         }
