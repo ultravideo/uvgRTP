23e62c42901bb1eed7857830ebc0b29f63d76e84
Joni Räsänen <joni.rasanen@iki.fi>
2022-09-08 11:47:28 +0300

    rtp: Fix holepuncher thread exiting

--- src/holepuncher.cc
+++ src/holepuncher.cc
@@ -16,29 +16,23 @@ uvgrtp::holepuncher::holepuncher(std::shared_ptr<uvgrtp::socket> socket):
 
 uvgrtp::holepuncher::~holepuncher()
 {
-    active_ = false;
-
-    if (runner_ != nullptr)
-    {
-        if (runner_->joinable())
-        {
-            runner_->join();
-        }
-        runner_ = nullptr;
-    }
+    stop();
 }
 
 rtp_error_t uvgrtp::holepuncher::start()
 {
-    runner_ = std::unique_ptr<std::thread> (new std::thread(&uvgrtp::holepuncher::keepalive, this));
-    runner_->detach();
     active_ = true;
+    runner_ = std::unique_ptr<std::thread> (new std::thread(&uvgrtp::holepuncher::keepalive, this));
     return RTP_OK;
 }
 
 rtp_error_t uvgrtp::holepuncher::stop()
 {
     active_ = false;
+    if (runner_ && runner_->joinable())
+    {
+        runner_->join();
+    }
     return RTP_OK;
 }
 
@@ -49,14 +43,19 @@ void uvgrtp::holepuncher::notify()
 
 void uvgrtp::holepuncher::keepalive()
 {
+    UVG_LOG_DEBUG("Starting holepuncher");
+
+    // TODO: Make this follow https://datatracker.ietf.org/doc/html/rfc6263
     while (active_) {
         if (uvgrtp::clock::ntp::diff_now(last_dgram_sent_) < THRESHOLD) {
             std::this_thread::sleep_for(std::chrono::milliseconds(500));
             continue;
         }
 
+        UVG_LOG_DEBUG("Sending keep-alive");
         uint8_t payload = 0x00;
         socket_->sendto(&payload, 1, 0);
         last_dgram_sent_ = uvgrtp::clock::ntp::now();
     }
+    UVG_LOG_DEBUG("Stopping holepuncher");
 }
