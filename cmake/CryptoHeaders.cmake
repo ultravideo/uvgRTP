03ee03cd0711b530011ab2d3f36ed67211668ea6
Joni Räsänen <joni.rasanen@iki.fi>
2023-05-02 13:29:33 +0300

    build: Add option to download Crypto++ headers to CMake

---
+++ cmake/CryptoHeaders.cmake
@@ -0,0 +1,18 @@
+include(FetchContent)
+
+message(STATUS "Downloading Crypto++ headers")
+
+FetchContent_Declare(cryptopp
+  URL https://github.com/weidai11/cryptopp/releases/download/CRYPTOPP_8_7_0/cryptopp870.zip
+  URL_HASH SHA256=d0d3a28fcb5a1f6ed66b3adf57ecfaed234a7e194e42be465c2ba70c744538dd
+  DOWNLOAD_EXTRACT_TIMESTAMP ON
+)
+
+FetchContent_MakeAvailable(cryptopp)
+
+file(GLOB CRYPTOPP_HEADERS "${cryptopp_SOURCE_DIR}/*.h")
+file(MAKE_DIRECTORY ${cryptopp_SOURCE_DIR}/include/cryptopp)
+file(COPY ${CRYPTOPP_HEADERS} DESTINATION ${cryptopp_SOURCE_DIR}/include/cryptopp)
+
+include_directories(${cryptopp_SOURCE_DIR}/include)
+
