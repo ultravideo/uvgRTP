228a282c35f6a527b9107c523286c2fe8685136b
Joni Räsänen <joni.rasanen@iki.fi>
2023-05-11 10:14:49 +0300

    build: Be more user friendly if git or pkgconfig are not found

--- cmake/CryptoHeaders.cmake
+++ cmake/CryptoHeaders.cmake
@@ -1,18 +1,23 @@
-include(FetchContent)
+find_package(Git)
 
-message(STATUS "Downloading Crypto++ headers")
+if (Git_FOUND)
+    include(FetchContent)
 
-FetchContent_Declare(cryptopp
-  URL https://github.com/weidai11/cryptopp/releases/download/CRYPTOPP_8_7_0/cryptopp870.zip
-  URL_HASH SHA256=d0d3a28fcb5a1f6ed66b3adf57ecfaed234a7e194e42be465c2ba70c744538dd
-  DOWNLOAD_EXTRACT_TIMESTAMP ON
-)
+    message(STATUS "Downloading Crypto++ headers")
 
-FetchContent_MakeAvailable(cryptopp)
+    FetchContent_Declare(cryptopp
+      URL https://github.com/weidai11/cryptopp/releases/download/CRYPTOPP_8_7_0/cryptopp870.zip
+      URL_HASH SHA256=d0d3a28fcb5a1f6ed66b3adf57ecfaed234a7e194e42be465c2ba70c744538dd
+      DOWNLOAD_EXTRACT_TIMESTAMP ON
+    )
 
-file(GLOB CRYPTOPP_HEADERS "${cryptopp_SOURCE_DIR}/*.h")
-file(MAKE_DIRECTORY ${cryptopp_SOURCE_DIR}/include/cryptopp)
-file(COPY ${CRYPTOPP_HEADERS} DESTINATION ${cryptopp_SOURCE_DIR}/include/cryptopp)
+    FetchContent_MakeAvailable(cryptopp)
 
-include_directories(${cryptopp_SOURCE_DIR}/include)
+    file(GLOB CRYPTOPP_HEADERS "${cryptopp_SOURCE_DIR}/*.h")
+    file(MAKE_DIRECTORY ${cryptopp_SOURCE_DIR}/include/cryptopp)
+    file(COPY ${CRYPTOPP_HEADERS} DESTINATION ${cryptopp_SOURCE_DIR}/include/cryptopp)
 
+    include_directories(${cryptopp_SOURCE_DIR}/include)
+else()
+    message(WARNING, "Git not found, not downloading Crypto++ headers!)
+endif()
