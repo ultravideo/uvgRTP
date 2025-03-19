b451ae264159074ce54f3259034ca7cbf897d283
Joni Räsänen <joni.rasanen@iki.fi>
2025-03-19 15:29:44 +0200

    tests: Update googletest version

--- cmake/FindDependencies.cmake
+++ cmake/FindDependencies.cmake
@@ -12,7 +12,7 @@ if (NOT UVGRTP_DISABLE_TESTS)
     FetchContent_Declare(
             googletest
             GIT_REPOSITORY https://github.com/google/googletest.git
-            GIT_TAG        release-1.11.0
+            GIT_TAG        6910c9d9165801d8827d628cb72eb7ea9dd538c5 # v1.16.0
     )
     # For Windows: Prevent overriding the parent project's compiler/linker settings
     set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
