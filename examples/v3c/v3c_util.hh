e027e80c30d349782dc59e194b2f6d48cc0248c0
Jaakko Laitinen <jaakko.laitinen@tuni.fi>
2025-04-04 17:41:49 +0300

    Improve v3c example

--- examples/v3c/v3c_util.hh
+++ examples/v3c/v3c_util.hh
@@ -14,7 +14,8 @@ enum V3C_UNIT_TYPE {
     V3C_GVD    = 3, // Geometry video data
     V3C_AVD    = 4, // Attribute video data
     V3C_PVD    = 5, // Packed video data 
-    V3C_CAD    = 6 // Common atlas data 
+    V3C_CAD    = 6, // Common atlas data
+    V3C_UNDEF  = -1 // Unit type not defined
 };
 
 enum CODEC {
@@ -25,11 +26,11 @@ enum CODEC {
 };
 
 constexpr int V3C_HDR_LEN = 4; // 32 bits for v3c unit header
+constexpr int SAMPLE_STREAM_HDR_LEN = 1; // 8 bits for sample stream headers
+constexpr uint8_t DEFAULT_ATLAS_NAL_SIZE_PRECISION = 2;
+constexpr uint8_t DEFAULT_VIDEO_NAL_SIZE_PRECISION = 4;
 
-// These are signaled to the receiver one way or the other, for example SDP
-constexpr uint8_t ATLAS_NAL_SIZE_PRECISION = 2;
-constexpr uint8_t VIDEO_NAL_SIZE_PRECISION = 4;
-constexpr uint8_t V3C_SIZE_PRECISION = 3;
+constexpr uint8_t MAX_V3C_SIZE_PREC = 8;
 
 struct vuh_ad {
     uint8_t vuh_v3c_parameter_set_id = 0;
@@ -71,6 +72,14 @@ struct v3c_unit_header {
         vuh_pvd pvd;
         vuh_cad cad;
     };
+
+    v3c_unit_header() {
+      std::memset(this, 0, sizeof(v3c_unit_header));
+    }
+
+    v3c_unit_header(const uint8_t unit_type) : v3c_unit_header() {
+      this->vuh_unit_type = unit_type;
+    }
 };
 
 struct nal_info {
@@ -83,6 +92,7 @@ struct nal_info {
  - nal_info struct holds the format(Atlas, H264, H265, H266), start position and size of the NAL unit
  - With this info you can send the data via different uvgRTP media streams. */
 struct v3c_unit_info {
+    uint8_t sample_stream_nal_precision;
     v3c_unit_header header;
     std::vector<nal_info> nal_infos = {};
     //char* buf; // (used on the receiving end)
@@ -108,10 +118,23 @@ struct v3c_streams {
     uvgrtp::media_stream* avd = nullptr;
 };
 
-uint32_t combineBytes(uint8_t byte1, uint8_t byte2, uint8_t byte3, uint8_t byte4);
-uint32_t combineBytes(uint8_t byte1, uint8_t byte2, uint8_t byte3);
-uint32_t combineBytes(uint8_t byte1, uint8_t byte2);
-void convert_size_big_endian(uint32_t in, uint8_t* out, size_t output_size);
+uint64_t combineBytes(const uint8_t *const bytes, const uint8_t num_bytes);
+void convert_size_big_endian(uint64_t in, uint8_t* out, size_t output_size);
+
+// Get sizes of different v3c unit types
+template <V3C_UNIT_TYPE E>
+uint64_t get_v3c_unit_size(const v3c_unit_info &info);
+uint64_t get_v3c_unit_size(const v3c_unit_info &info, const uint8_t unit_type);
+template <>
+uint64_t get_v3c_unit_size<V3C_VPS>(const v3c_unit_info &info);
+template <>
+uint64_t get_v3c_unit_size<V3C_AD>(const v3c_unit_info &info);
+template <>
+uint64_t get_v3c_unit_size<V3C_OVD>(const v3c_unit_info &info);
+template <>
+uint64_t get_v3c_unit_size<V3C_GVD>(const v3c_unit_info &info);
+template <>
+uint64_t get_v3c_unit_size<V3C_AVD>(const v3c_unit_info &info);
 
 // Get size of a file in bytes
 uint64_t get_size(std::string filename);
@@ -129,18 +152,18 @@ void parse_v3c_header(v3c_unit_header &hdr, char* buf, uint64_t ptr);
 v3c_streams init_v3c_streams(uvgrtp::session* sess, uint16_t src_port, uint16_t dst_port, int flags, bool rec);
 
 // Initialize a memory map of a V3C file
-v3c_file_map init_mmap();
+v3c_file_map init_mmap(uint8_t atlas_nal_precision = DEFAULT_ATLAS_NAL_SIZE_PRECISION, uint8_t video_nal_precision = DEFAULT_VIDEO_NAL_SIZE_PRECISION);
 
 // Used in receiver_hooks to copy the received data
 void copy_rtp_payload(std::vector<v3c_unit_info>* units, uint64_t max_size, uvgrtp::frame::rtp_frame* frame);
 
 // Combine a complete V3C unit from received NAL units
-void create_v3c_unit(v3c_unit_info& current_unit, char* buf, uint64_t& ptr, uint64_t v3c_precision, uint32_t nal_precision);
+void create_v3c_unit(v3c_unit_info& current_unit, char* buf, uint64_t& ptr, uint8_t v3c_precision);
 
 // Reconstruct a whole GOF from V3C Units
-uint64_t reconstruct_v3c_gof(bool hdr_byte, char* &buf, uint64_t& ptr, v3c_file_map& mmap, uint64_t index);
+uint64_t reconstruct_v3c_gof(bool hdr_byte, char* &buf, uint64_t& ptr, v3c_file_map& mmap, uint64_t index, uint8_t v3c_precision);
 
 // Check if there is a complete GOF in the memory map
 bool is_gof_ready(uint64_t index, v3c_file_map& mmap);
 
-uint64_t get_gof_size(bool hdr_byte, uint64_t index, v3c_file_map& mmap);
+uint64_t get_gof_size(bool hdr_byte, uint64_t index, v3c_file_map& mmap, uint8_t v3c_precision);
